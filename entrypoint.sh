c37a4756aa0ac60322254a705239c3ad6919e8cd
Patrick <166351017+patrwongadr@users.noreply.github.com>
2024-09-13 17:16:59 +1000

    ADA-4459: fix python docker container

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ export ORG_OU_IDS=$5
 export ACCOUNT_IDS=$6
 export REGIONS=$7
 
-. /./venv/bin/activate
-python /stack_set_helper_action/main.py
+. /app/venv/bin/activate
+python stack_set_helper_action/main.py
